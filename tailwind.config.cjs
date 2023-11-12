8895b339a85d5834c880f5e3b1131bdf8fec95f0
Tinka8 <lenmatka818@gmail.com>
2023-11-12 22:48:05 +0100

    aterials
    materials

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,9 +7,10 @@ module.exports = {
       },
     extend: {
       colors: {
-          "transparent-white": "rgba(255, 255, 255, 0.87)",
-          "primary": "#7e01ff",
-          "secondary" : "#66ffcc",
+            "transparent-white": "rgba(255, 255, 255, 0.87)",
+            "primary": "#7e01ff",
+            "secondary": "#66ffcc",
+            "emphasis": "#ff9494",
       },
     },
   },
